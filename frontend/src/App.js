316e95d997043c58bb4fec954dc730be6d54c049
Jake Dibbert <jake.dibbert@sofology.co.uk>
2021-03-09 12:04:24 +0000

    template start

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Navbar from '../src/components/UI/Navbar/Navbar'
+import Center from '../src/components/UI/Center/Center'
+import Sidebar from '../src/components/UI/Sidebar/Sidebar'
+import Footer from '../src/components/UI/Footer/Footer'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* Navbar */}
+      {/* Center */}
+      {/* Sidebar */}
+      {/* Footer */}
     </div>
-  );
+  )
 }
 
 export default App;
