d5c078821a094310e92b8300f0b29d99081aab1a
Gemcila <ggemcila@gmail.com>
2021-03-10 13:14:16 -0800

    #6 Added images (props)

--- frontend/src/Components/Center-reviews/CenterItems.js
+++ frontend/src/Components/Center-reviews/CenterItems.js
@@ -1,9 +1,17 @@
 const CenterItems = (props) => {
     return(
         <div>
+
+            <div>
+            <img alt="centerImage" src={props.centerimage} className="CenterImage" />
+            </div>
+
+            <div className="Reviews container"> 
             <h2>{props.name}</h2>
             <div className="date">{props.date}</div>
             <div className="description">{props.description}</div>
+            </div>
+
         </div>
     );
 }
