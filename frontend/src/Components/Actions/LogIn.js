1196117dc72414f85ed4110dd830a88b69c0e498
jordan <jthomasw88>
2021-03-11 19:51:48 +0000

    s

--- frontend/src/Components/Actions/LogIn.js
+++ frontend/src/Components/Actions/LogIn.js
@@ -5,15 +5,10 @@ const LogIn = () => {
   return (
     <ul>
       <li>
-        <button className="LOG">
-          log In
-          <input type="email"></input>
-          <input type="password"></input>
-        </button>
+        <button className="LOG">log In</button>
       </li>
-      <li>
-        <button className="SU">Sign Up</button>
-      </li>
+      <input type="email" placeholder="Username" className="input"></input>
+      <input type="password" placeholder="password" className="input"></input>
     </ul>
   );
 };
