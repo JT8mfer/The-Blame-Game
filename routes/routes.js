33d00651253f8bbdc6a0a41cd8288e7de9dc64ee
Lisa Rawlings <lisa-rawlings@hotmail.com>
2021-03-11 14:36:13 +0000

    Backend

---
+++ routes/routes.js
@@ -0,0 +1,108 @@
+// const express = require("express");
+// const User = require("../models/user");
+// const router = express.Router();
+// router.get("/:name", (req, res) => {
+//   User.find({ name: req.params.name }, (err, obj) => {
+//     if (err) {
+//       console.log(err);
+//       res.status(500).json({ msg: err });
+//     } else {
+//       res.status(200).json(obj);
+//     }
+//   });
+// });
+// router.post("/", (req, res) => {
+//   const data = {
+//     userName: req.body.name,
+//     email: req.body.email,
+//     password: req.body.password,
+//   };
+//   new User(data).save((err, obj) => {
+//     if (err) {
+//       res.status(500).json({ msg: err });
+//     } else {
+//       res.status(200).json(obj);
+//     }
+//   });
+// });
+// router.delete("/:name", (req, res) => {
+//   User.deleteOne({ name: req.params.name }, (err, obj) => {
+//     if (err) {
+//       console.log(err);
+//       res.status(500).json({ msg: err });
+//     } else {
+//       res.status(200).json(obj);
+//     }
+//   });
+// });
+// module.exports = router;
+
+const express = require("express");
+const router = express.Router();
+
+const User = require("../models/user.js");
+const saltRounds = 10;
+const bcrypt = require("bcrypt");
+
+router.get("/:userName", (req, res) => {
+  User.find({ userName: req.params.userName }, (err, obj) => {
+    if (err) {
+      console.log(err);
+      res.status(500).json({ msg: err });
+    } else if (obj) {
+      res.status(200).json(obj);
+    } else {
+      res.status(404).json({ msg: "Not found" });
+    }
+  });
+});
+
+router.post("/", async (req, res) => {
+  if (req.body.password === req.body.verifyPassword) {
+    const data = {
+      userName: req.body.userName,
+      email: req.body.email,
+      password: await bcrypt.hash(req.body.password, saltRounds),
+    };
+    new User(data).save((err, obj) => {
+      if (err) {
+        res.status(500).json({ msg: err });
+      } else {
+        res.status(200).json(obj);
+      }
+    });
+    save((err, obj) => {
+      if (err) {
+        res.status(200);
+        res.send({
+          Status: "ERROR",
+          message:
+            "Oops! This email is already in use. Please enter another and try again",
+        });
+      } else {
+        res.status(200);
+        res.send({
+          Status: "WORKING",
+          Email: req.body.email,
+          Password: req.body.password,
+        });
+      }
+    });
+  } else if (req.body.password !== req.body.verifyPassword) {
+    res.status(200);
+    res.send({ Status: "ERROR", message: "Your passwords do not match" });
+  }
+});
+
+// router.delete("/:userName", async (req, res) => {
+//   User.deleteOne({ email }, (err, user) => {
+//     if (err) {
+//       console.log(err);
+//       res.status(200).json({ msg: err });
+//     } else {
+//       res.status(200).json(obj);
+//     }
+//   });
+// });
+
+module.exports = router;
