58ad25f53fa550419ead3279716cab7aec47c27b
Gemcila <ggemcila@gmail.com>
2021-03-11 18:47:25 -0800

    #6 Stlying comment button

--- frontend/src/Components/Center-reviews/CenterItems.js
+++ frontend/src/Components/Center-reviews/CenterItems.js
@@ -13,7 +13,7 @@ const CenterItems = (props) => {
             <h2 className="titleReview">{props.name}</h2>
             <div className="date">{props.date}</div>
             <div className="description">{props.description}</div>
-            <div className="commentIcon"><button className="commentButton"><AiOutlineMessage size="30px"/></button></div>
+            <div className="commentIcon"><button className="commentButton"><AiOutlineMessage size="25px"/></button></div>
             </div>
 
         </div>
