98583b616458420ec27deb4671e0df5c003edfd5
Gemcila <ggemcila@gmail.com>
2021-11-18 18:28:02 +0000

    #Switch into Routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Sidebar from "./Components/Sidebar";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Overview from "./pages/Overview";
 import { Reports, ReportsOne, ReportsTwo, ReportsThree } from "./pages/Reports";
 import Team from "./pages/Team";
@@ -14,7 +14,7 @@ const App = () => {
     <div className="mainBody">
       <Router>
         <Sidebar />
-        <Switch>
+        <Routes>
           <Route path="/overview" exact component={Overview} />
           <Route path="/reports" exact component={Reports} />
           <Route path="/reports/reports1" exact component={ReportsOne} />
@@ -24,7 +24,7 @@ const App = () => {
           <Route path="/signup" exact component={SignUp} />
           <Route path="/" exact component={Home} />
           <Route path="/login" exact component={Login} />
-        </Switch>
+        </Routes>
       </Router>
       {/* <LogIn /> */}
 
